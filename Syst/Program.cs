b7f59b39c667dd244695fd71a6c30e2be8127d05
Camille <cgon@itu.dk>
2022-03-09 15:56:38 +0100

    still working wuhu

--- Syst/Program.cs
+++ Syst/Program.cs
@@ -1,4 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Syst;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var builderDB = new DbContextOptionsBuilder<SystematicContext>();
+        
+using (var db = new SystematicContextFactory().CreateDbContext(new string[0]))
+{
+    db.Database.EnsureCreated();
+    var repoCandidate = new CandidateRepository(db);
+    var repoAdmin = new AdminRepository(db);
+    var repoEvent = new EventRepository(db);
+    var repoQuestions = new QuestionRepository(db);
+    var repoQuiz = new QuizRepository(db);
+}
 
 // Add services to the container.
 
