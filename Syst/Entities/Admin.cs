2614542fa294f08b3dc0fdcee8f457115ca09124
Lukas <90245364+EatOreo@users.noreply.github.com>
2022-03-10 15:41:16 +0100

    Delete Syst/Entities directory

--- Syst/Entities/Admin.cs
+++
@@ -1,9 +0,0 @@
-namespace Syst{
-
-    public class Admin{
-        public int Id {get; set;}
-        public string? Name {get; set;}
-        public string? Email {get; set;}
-        public ICollection<Event>? Events {get;set;}
-    }
-}
