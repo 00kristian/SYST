11bca13785e150a9b239fac843f12d4741d708ee
luhj <luhj@itu.dk>
2022-03-10 20:09:21 +0100

    Ayo fetching works

--- Syst/ClientApp/src/setupProxy.js
+++ Syst/ClientApp/src/setupProxy.js
@@ -5,7 +5,8 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:41505';
 
 const context =  [
-  "/weatherforecast",
+  "/swagger",
+  "/api",
 ];
 
 module.exports = function(app) {
