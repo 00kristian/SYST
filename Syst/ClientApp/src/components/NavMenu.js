caa1c7c861277f2fc21c12257c9605cc5d9005e0
Camille <cgon@itu.dk>
2022-03-11 09:31:21 +0100

    Added logos

--- Syst/ClientApp/src/components/NavMenu.js
+++ Syst/ClientApp/src/components/NavMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import logo from './Systematic_Logo.png';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -26,7 +27,7 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">Syst</NavbarBrand>
+            <img src={logo} alt="Logo" width={230}/>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
