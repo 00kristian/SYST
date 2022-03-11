8f511999c65dbe5615c6cbb254c697de99c2e8ab
camillegonnsen <90042303+camillegonnsen@users.noreply.github.com>
2022-03-11 13:14:54 +0100

    small fix (#15)

--- Syst/ClientApp/src/components/NavMenu.js
+++ Syst/ClientApp/src/components/NavMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Container, Navbar, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 import logo from './Systematic_Logo.png';
