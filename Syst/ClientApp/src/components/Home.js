f801edd17458c7d6fde3b59fb9deef3d54776967
sarahcecilie <90042239+sarahcecilie@users.noreply.github.com>
2022-03-18 14:29:49 +0100

    Homepage (#28)
    * Homepage has events (rating doesn't work)
    
    * Rating works now (Fixed in EventRepository)
    
    Co-authored-by: sacc <sacc@itu.dk>

--- Syst/ClientApp/src/components/Home.js
+++ Syst/ClientApp/src/components/Home.js
@@ -1,58 +1,63 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-render() {
+    constructor(props) {
+        super(props);
+        this.state = { events: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateData();
+    }
+
+    static renderEventsTable(events) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Date</th>
+                    <th>Location</th>
+                    <th>Rating</th>
+                </tr>
+                </thead>
+                <tbody>
+                {events.map(event =>
+                    <tr key={event.id}>
+                        <td>{event.id}</td>
+                        <td>{event.name}</td>
+                        <td>{event.date}</td>
+                        <td>{event.location}</td>
+                        <td>{event.rating}</td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
         let contents = this.state.loading
             ? <p><em>Loading...</em></p>
-            : Home.renderAdminsTable(this.state.admins);
+            : Home.renderEventsTable(this.state.events);
 
         return (
             <div>
                 <h1>Welcome to Systematic Event Tool!</h1>
-                <p>From this home page you'll be able to create, host and see and overview over events! Check it out!</p>
-
+                <p>From this home page you'll be able to create, host and see and overview over events! Check it out!</p>                
+                
                 <h1 id="tabelLabel" >Events</h1> <button className="btn btn-primary">Create</button>
                 {contents}
             </div>
-        );}
+        );
+    }
 
-        constructor(props) {
-          super(props);
-          this.state = { admins: [], loading: true };
-        }
-      
-        componentDidMount() {
-          this.populateData();
-        }
-
-        static renderAdminsTable(admins) {
-          return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-              <thead>
-                <tr>
-                  <th>Id</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                </tr>
-              </thead>
-              <tbody>
-                {admins.map(admin =>
-                  <tr key={admin.id}>
-                    <td>{admin.id}</td>
-                    <td>{admin.name}</td>
-                    <td>{admin.email}</td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          );
-        }
-
-        async populateData() {
-          const response = await fetch('api/admins/1');
-          const data = await response.json();
-          this.setState({ admins: [data], loading: false });
-        }
+    async populateData() {
+        const response = await fetch('api/events');
+        const data = await response.json();
+        this.setState({ events: data, loading: false });
+    }
 }
