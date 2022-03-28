8cb11f8e30227a70787f74b448716c145a20c420
sacc <sacc@itu.dk>
2022-03-28 22:19:46 +0200

    Logo links to homepage

--- Syst/ClientApp/src/components/NavMenu.js
+++ Syst/ClientApp/src/components/NavMenu.js
@@ -27,7 +27,9 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <img src={logo} alt="Logo" width={230}/>
+            <a href={"/"}>
+              <img src={logo} alt="Logo" width={230}/>
+            </a>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
