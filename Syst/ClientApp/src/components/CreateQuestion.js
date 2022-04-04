fad4beb6b89df3fda702d5937ac80d1d843b8c16
00kristian <90248771+00kristian@users.noreply.github.com>
2022-04-04 09:51:37 +0200

    Createquizimage (#66)
    * Created QuestionsController
    
    * NOT WORKING
    
    * More tests
    
    * More tests (Works now)
    
    * More tests
    
    * Sat Createquiz og createquestion op
    
    * Skulle lige over i main
    
    * Dropdown fungerer men ikke pæn
    
    * Tror CreateQuestion er færdig minus image
    
    Co-authored-by: sacc <sacc@itu.dk>
    Co-authored-by: Kristian Berlin <krbj@itu.dk>
    Co-authored-by: sarahcecilie <90042239+sarahcecilie@users.noreply.github.com>

---
+++ Syst/ClientApp/src/components/CreateQuestion.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import Dropdown from 'react-dropdown';
+
+
+export class CreateQuestion extends Component {
+    static displayName = CreateQuestion.name;
+
+    constructor(props) {
+        super(props);
+        this.state = {  Representation: "", Answer: "", Options: ["", "", "", ""], imageUrl: ""};
+    }
+
+    render() {
+        const options = ["A", "B", "C", "D"];
+        const defaultOption = options[0];
+
+        return (
+            <div>
+                <h2>Here you can create a question </h2>
+                <br/>
+                <form>
+                <label>
+                    <h5>Representation</h5>
+                    <input className="input-field" onChange={(event) => this.state.Representation = event.target.value}></input>
+                    </label>
+                    <br />
+                    <br />
+                
+                
+                    <form >
+                    <label>
+                        <h5 id='a'>Q1: Answer A</h5>
+                        <input className="input-field" onChange={(event) => this.state.Options[0] = event.target.value}></input>
+                    </label>
+                    <label>
+                    <h5 id='b'>Q1: Answer B</h5>
+                    <input className="input-field" onChange={(event) => this.state.Options[1] = event.target.value}></input>
+                    </label>
+                    <label>
+                    <br />
+                    <h5 id='c'>Q1: Answer C</h5>
+                    <input className="input-field" onChange={(event) => this.state.Options[2] = event.target.value}></input>
+                    </label>
+                    <label>
+                    <h5 id='d'>Q1: Answer D</h5>
+                    <input className="input-field" onChange={(event) => this.state.Options[3] = event.target.value}></input>
+                    </label>
+                    </form>
+
+                    <br />
+                    <br />
+
+                    <button className="btn btn-primary leftbtn" >Upload Image </button>
+
+                
+                </form>
+                <br />
+                <br />
+                <br />
+                <h5>Correct Answer</h5>
+                <Dropdown options={options} onChange={this.selectAnswer} value={defaultOption} placeholder="Choose Answer" />
+
+
+                <button className="btn btn-primary leftbtn" onClick={this.rerouteToCreateEvent}>Confirm </button>
+                <button className="btn btn-primary rightbtn" onClick={this.rerouteToQuiz}>Cancel</button>
+            </div>
+        );
+    }
+
+    rerouteToCreateEvent = () => {
+        let event = {
+            "representation": this.state.Representation,
+            "answer": this.state.Answer,
+            "Options": this.state.Options,
+            "imageUrl": this.state.imageUrl
+        };
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(event)
+        };
+        fetch('api/questions', requestOptions)
+        .then(response => response.json())
+        const { history } = this.props;
+        history.push("/CreateQuiz");
+    }
+   
+
+    rerouteToQuiz = () => {
+        const { history } = this.props;
+        history.push("/CreateQuiz");
+    }
+
+    selectAnswer = (option) => {
+        this.setState({Answer: option.value});        
+    }
+
+    
+}
