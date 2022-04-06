846cb500509a24eccccc0fd11d2b920341b6f9d1
imag6 <isabellahmagnusdottir@gmail.com>
2022-04-06 20:27:19 +0200

    fix af create question

--- Syst/ClientApp/src/components/CreateQuestion.js
+++ Syst/ClientApp/src/components/CreateQuestion.js
@@ -9,13 +9,16 @@ export class CreateQuestion extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputValues: [{Representation: "", Answer: "", Options: "", imageUrl: ""}]
+            Representation: "",
+            Answer: "",
+            inputValues: [{OptionName: ""}],
+            ImageUrl: ""
         };
     }
 
     addOptionFields() {
         this.setState(({
-            inputValues: [...this.state.inputValues, {Answer: "", Options: ""}]
+            inputValues: [...this.state.inputValues, {OptionName:""}]
         }))
     }
 
@@ -27,8 +30,7 @@ export class CreateQuestion extends Component {
 
     render() {
 
-        const options = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Æ", "Ø", "Å"];
-        const defaultOption = options[0];
+        const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Æ", "Ø", "Å"];
 
         return (
             <div class="CreateQPage">
@@ -37,7 +39,7 @@ export class CreateQuestion extends Component {
                 <form>
                     <label>
                         <h5>Question</h5>
-                        <input className="input-field" onChange={(event) => this.state.Representation = event.target.value} />
+                        <input className="input-field q-field" onChange={(event) => this.state.Representation = event.target.value} />
                     </label>
                     
                     <br />
@@ -45,15 +47,23 @@ export class CreateQuestion extends Component {
                     {this.state.inputValues.map((answer, index) =>
                         <div key={index}>
                             <label>
-                                <h5>Option {options[index]}</h5>
-                                <label>Correct answer?</label>
-                                <input type = "radio" name="correctAnswer" onClick={(event) => this.state.answer = event.target.value}/>
-                            <input className= "input-field" onChange={(event) => this.state.Options[index] = event.target.value} />
+                                <div className="flex-container">
+                                    <h5 className="flex-child">Option {letters[index]}</h5>
+                                    <div className="flex-child correctAns">
+                                        <label className="cText">Correct answer?</label>
+                                        <input type = "radio" name="correctAnswer" onChange={this.state.answer = letters[index]}/>
+                                    </div>
+                                </div>
+                            <input className= "input-field" onChange={(event) => this.state.inputValues.OptionName = event.target.value } />
+                                <button className="btn btn-primary" type="button" onClick={() => this.removeOptionFields()}>-</button>
+
                             </label>
                         </div>
                         )}
                         <button className="btn btn-primary" type="button" onClick={() => this.removeOptionFields()}>-</button>
-                        <button className="btn btn-primary" type="button" onClick={() => this.addOptionFields()}>+</button>
+                        <br />
+                        <button className="btn btn-plus" type="button" onClick={() => this.addOptionFields()}>+</button>
+                    <br />
                     <br />
                     <h5>Select an image for the question</h5>
                     <button className="btn btn-primary leftbtn" >Upload Image</button>
@@ -69,10 +79,10 @@ export class CreateQuestion extends Component {
 
     rerouteToCreateEvent = () => {
         let event = {
-            "representation": this.state.Representation,
-            "answer": this.state.Answer,
-            "Options": this.state.Options,
-            "imageUrl": this.state.imageUrl
+            "representation": this.state.inputValues.Representation,
+            "answer": this.state.inputValues.Answer,
+            "Options": this.state.inputValues.Options,
+            "imageUrl": this.state.inputValues.imageUrl
         };
         console.log(event);
         const requestOptions = {
@@ -81,7 +91,7 @@ export class CreateQuestion extends Component {
             body: JSON.stringify(event)
         };
         fetch('api/questions', requestOptions)
-        .then(response => response.json())
+            .then(response => response.json())
         const { history } = this.props;
         history.push("/CreateQuiz/"+this.props.match.params.quiz_id+ "/"+ this.props.match.params.quiz_id);
     }
