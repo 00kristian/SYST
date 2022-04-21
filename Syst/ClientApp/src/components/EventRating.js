7b71e0a7dc16f6060ee78384a21b14176ce03dbe
Maj Frost Jensen <maj@frost-jensen.dk>
2022-04-21 13:27:24 +0200

    Added dropdown menus to rating event

--- Syst/ClientApp/src/components/EventRating.js
+++ Syst/ClientApp/src/components/EventRating.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Dropdown from 'react-dropdown';
 
 export class EventRating extends Component {
     static displayName = EventRating.name;
@@ -14,15 +15,53 @@ export class EventRating extends Component {
         );
     }
     
-    render(){
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : EventRating.renderRating()
+    render() {
+        const QuestionCandidates = [
+            '1 - None',
+            '2 - Fewer than expected',
+            '3 - As many as expected',
+            '4 - A few more than expected',
+            '5 - Far more than expected'
+        ];
+        const QuestionApplications = [
+            '1 - None',
+            '2 - Fewer than expected',
+            '3 - As many as expected',
+            '4 - A few more than expected',
+            '5 - Far more than expected'
+        ];
+        const QuestionCost = [
+            '1 - Free or included in partnership',
+            '2 - Less than 10.000 kr.',
+            '3 - 11.000 to 25.000 kr.',
+            '4 - 26.000 to 40.000 kr.',
+            '5 - More than 41.000 kr.'
+        ];
+        const QuestionTime = [
+            '1 - None',
+            '2 - Fewer than expected',
+            '3 - As many as expected',
+            '4 - A few more than expected',
+            '5 - Far more than expected'
+        ];
+
+
         return (
-        <div>
-          <h1>Hello</h1>
-          {contents}
-        </div>
+            <div>
+                <h2>Rating</h2>
+                <p>1. How many relevant candidates did we meet?</p>
+                <Dropdown options={QuestionCandidates} value="Select Rating" />
+                <br/>
+                <p>2. How many applications do we expect to receive after the event?</p>
+                <Dropdown options={QuestionApplications} value="Select Rating" />
+                <br/>
+                <p>3. What was the cost of the event?</p>
+                <Dropdown options={QuestionCost} value="Select Rating" />
+                <br/>
+                <p>4. How much time did we invest in preparing for the event?</p>
+                <Dropdown options={QuestionTime} value="Select Rating" />
+           
+            </div>
         );
     }
 
