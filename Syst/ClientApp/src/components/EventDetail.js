3502bb569d49c6c72561166cf0b1b3af4165e0fb
Camille <cgon@itu.dk>
2022-04-28 09:21:20 +0200

    small fix

--- Syst/ClientApp/src/components/EventDetail.js
+++ Syst/ClientApp/src/components/EventDetail.js
@@ -22,7 +22,6 @@ export class EventDetail extends Component {
             <h2>{event.date}</h2>
             <h2>{event.location}</h2>
             <h2>Winner: { winnerName  }</h2>
-            <h2 className='txt-left'>{event.location}</h2>
             <h2 className='txt-right'>Rating: {event.rating}</h2>
             <br/>
             <br/>
