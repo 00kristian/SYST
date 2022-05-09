6331b365d3832e9d828d5033e935c04d49214345
Isabella Magnusdottir <90042116+imag6@users.noreply.github.com>
2022-05-09 09:44:14 +0200

    Fix/fakemain (#154)
    * Man kan logge ind
    
    * INH-48 tryout this jira thing
    
    * INH-126 motherfucker
    
    * INH-126 Fiksede fejl med useauth
    
    * INH-129 har fundet templaten for authenticated
    
    Co-authored-by: Kristian Berlin <krbj@itu.dk>

--- Syst/ClientApp/src/components/NavMenu.js
+++ Syst/ClientApp/src/components/NavMenu.js
@@ -1,41 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Collapse, Container, Navbar, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 import logo from './Systematic_Logo.png';
 import Icon from '@mdi/react'
-import { mdiHome } from '@mdi/js'
+import { mdiCloseOutline, mdiHome } from '@mdi/js'
 import { mdiCalendar } from '@mdi/js';
 import { mdiAccountGroup } from '@mdi/js';
+import { useIsAuthenticated } from "@azure/msal-react";
+import { SignInButton } from "./SignInButton";
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+export function NavMenu(props) {
+  //static displayName = NavMenu.name;
+  const [collapsed, setCollapsed] = useState(true);
+  const isAuthenticated = useIsAuthenticated();
+  console.log(isAuthenticated);
 
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
   }
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+ // setIsAuthenticated(useIsAuthenticated());
 
-  render () {
     return (
+      
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
             <a href={"/"}>
               <img src={logo} alt="Logo" width={230}/>
             </a>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+            <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
                   <NavLink tag={Link} className="navitem text-dark" to="/"><Icon path={mdiHome} size={1}/> Home</NavLink>
@@ -46,11 +42,14 @@ export class NavMenu extends Component {
                 <NavItem>
                   <NavLink tag={Link} className="navitem text-dark" to="/candidates"><Icon path={mdiAccountGroup} size={1}/> Candidates</NavLink>
                 </NavItem>
+                <NavItem>
+                { !isAuthenticated ? <SignInButton></SignInButton> : <h1>Signed in </h1> }              
+                  </NavItem>
               </ul>
             </Collapse>
           </Container>
         </Navbar>
       </header>
     );
-  }
+  
 }
---
+++ Syst/ClientApp/src/components/SignInButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useMsal } from "@azure/msal-react";
+import { loginRequest } from "../authConfig";
+
+function handleLogin(instance) {
+    instance.loginRedirect(loginRequest).catch(e => {
+        console.error(e);
+    });
+}
+
+/**
+ * Renders a button which, when selected, will redirect the page to the login prompt
+ */
+export const SignInButton = () => {
+    const { instance } = useMsal();
+
+    return (
+        <button variant="secondary" className="ml-auto btn"  onClick={() => handleLogin(instance)}>Sign in using Redirect</button>
+    );
+}
