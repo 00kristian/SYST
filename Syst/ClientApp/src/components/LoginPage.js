08a3228c51c467bdd5b2a42eee1beea385e913f8
Camille <cgon@itu.dk>
2022-05-09 11:53:20 +0200

    INH-126 Background image added

--- Syst/ClientApp/src/components/LoginPage.js
+++ Syst/ClientApp/src/components/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './Systematic_Logo.png';
+import Systematic from './SystematicTower.png';
 import { SignInButton } from "./SignInButton";
 
 export class LoginPage extends Component {
@@ -18,13 +19,22 @@ export class LoginPage extends Component {
         let contents = this.state.loading
             ? <p><em>Loading...</em></p>
             : <SignInButton></SignInButton>
-
+        
+        const myStyle={
+                backgroundImage: `url(${Systematic})`,
+                height:'100vh',
+                
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+            };
         return (
+            <div style={myStyle}>
             <div className='div-center'>
                 <img style={{width: 600}} src={logo} alt="logo" />
                 <br></br>
                 <h1>Welcome to Event Tool</h1>
                 {contents}
+            </div>
             </div>
             
     );
