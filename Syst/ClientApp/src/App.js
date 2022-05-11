f5926b3a50f9b05e6cf12a6f7cdd98dde18ce626
Kristian Berlin <krbj@itu.dk>
2022-05-11 16:05:19 +0200

    INH-129 Fikset et problem med login

--- Syst/ClientApp/src/App.js
+++ Syst/ClientApp/src/App.js
@@ -68,37 +68,3 @@ export default class App extends Component {
     );
   }
 }
-
-function ProfileContent() {
-  const { instance, accounts } = useMsal();
-  const [graphData, setGraphData] = useState(null);
-
-  const name = accounts[0] && accounts[0].name;
-
-  function RequestProfileData() {
-      const request = {
-          ...loginRequest,
-          account: accounts[0]
-      };
-
-      // Silently acquires an access token which is then attached to a request for Microsoft Graph data
-      instance.acquireTokenSilent(request).then((response) => {
-          callMsGraph(response.accessToken).then(response => setGraphData(response));
-      }).catch((e) => {
-          instance.acquireTokenPopup(request).then((response) => {
-              callMsGraph(response.accessToken).then(response => setGraphData(response));
-          });
-      });
-  }
-
-  return (
-      <>
-          <h5 className="card-title">Welcome {name}</h5>
-          {graphData ? 
-              <ProfileData graphData={graphData} />
-              :
-              <Button variant="secondary" onClick={RequestProfileData}>Request Profile Information</Button>
-          }
-      </>
-  );
-};
