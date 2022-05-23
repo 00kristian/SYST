e3893b6ddf063f3856479ff50b47ea3bfe49fd1c
luhj <luhj@itu.dk>
2022-05-23 14:24:25 +0200

    INH-137 fix graduation validation

--- Syst/ClientApp/src/components/CandidateInformation.js
+++ Syst/ClientApp/src/components/CandidateInformation.js
@@ -15,7 +15,7 @@ export function CandidateInformation(props) {
     const [university, setUniversity] = useState("");
     const [studyProgram, setStudyProgram] = useState("");
     const [currentDegree, setCurrentDegree] = useState("");
-    const [graduationDate, setGraduationDate] = useState(new Date());
+    const [graduationDate, setGraduationDate] = useState(null);
 
     const [showSpecialUni, setShowSpecialUni] = useState(false);
     const [validateName, setValidateName] = useState(true);
@@ -23,6 +23,7 @@ export function CandidateInformation(props) {
     const [validateStudyProgram, setValidateStudyProgram] = useState(true);
     const [validateCheckBox, setValidateCheckBox] = useState(true);
     const [validateUniversity, setValidateUniversity] = useState(true);
+    const [validateGraduation, setValidateGraduation] = useState(true);
     const [validateDegree, setValidateDegree] = useState(true);
     const [clickedOnSubmit, setClickedOnSubmit] = useState(false);
     const { instance, accounts } = useMsal();
@@ -83,6 +84,7 @@ export function CandidateInformation(props) {
         const DegreeGood = currentDegree.length !== 0;
         const StudyProgramGood = studyProgram.length !== 0;
         const CheckBoxGood = validateCheckBox;
+        const GraduationGood = graduationDate != null;
 
         setValidateName(name.length !== 0);
         setValidateEmail(email.length !== 0);
@@ -90,8 +92,9 @@ export function CandidateInformation(props) {
         setValidateDegree(currentDegree.length !== 0);
         setValidateStudyProgram(studyProgram.length !== 0);
         setValidateCheckBox(validateCheckBox)
+        setValidateGraduation(GraduationGood)
 
-        if(NameGood && EmailGood && UniversityGood && DegreeGood && StudyProgramGood && CheckBoxGood) {
+        if(NameGood && EmailGood && UniversityGood && DegreeGood && StudyProgramGood && CheckBoxGood && GraduationGood) {
             
             let candidate = {
                 "name": name,
@@ -234,7 +237,8 @@ export function CandidateInformation(props) {
                 )}
                 <br />
                 <label>
-                    <h5>Graduation Month</h5>
+                    {validateGraduation ? <h5>Graduation Month</h5> : <h5 className='txt-red'>* Graduation Month</h5>}
+                    
                     <input onInput={(v) => {
                         setGraduationDate(new Date(v.target.value + "-01"));
                     }} 
